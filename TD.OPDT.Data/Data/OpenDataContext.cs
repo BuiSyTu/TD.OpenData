66d22b96d77760186154000d7ba38d386efed13a
Tùng Lâm Nguyễn <darkangelkid1109@gmail.com>
2021-02-01 15:04:30 +0700

    init

---
+++ TD.OPDT.Data/Data/OpenDataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD.Core.Api.Mvc;
+using TD.OPDT.Data.Models;
+
+namespace TD.OPDT.Data.Data
+{
+    public class OpenDataContext : TDCoreDbContext
+    {
+        public OpenDataContext() : base("DbContext")
+        {
+        }
+
+        public DbSet<DataSet> DataSets { get; set; }
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Office> Offices { get; set; } 
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DataSet>().ToTable("DataSet");
+            modelBuilder.Entity<Field>().ToTable("Field");
+            modelBuilder.Entity<Office>().ToTable("Office");
+            modelBuilder.Entity<User>().ToTable("User");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
